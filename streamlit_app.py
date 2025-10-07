303ca0b2c1d7fdebfff1e30e57b5972aeae22f35
Natalie Douglas <93133873+NatalieDouglas@users.noreply.github.com>
2025-10-07 13:18:46 +0100

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title("TRUTHS")
+st.write(
+    "DIRTT: Data assimilation for Inverting Radiative Transfer models using Truths data"
+)
+
+all_users = ["Alice", "Bob", "Charly"]
+with st.container(border=True):
+    users = st.multiselect("Users", all_users, default=all_users)
+    rolling_average = st.toggle("Rolling average")
+
+np.random.seed(42)
+data = pd.DataFrame(np.random.randn(20, len(users)), columns=users)
+if rolling_average:
+    data = data.rolling(7).mean().dropna()
+
+tab1, tab2 = st.tabs(["Chart", "Dataframe"])
+tab1.line_chart(data, height=250)
+tab2.dataframe(data, height=250, use_container_width=True)
